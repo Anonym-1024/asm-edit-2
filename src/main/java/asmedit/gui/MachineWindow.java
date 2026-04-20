f915020e734bbfdd24694544b1179981b8cb68f6
Vaclav Koukola <vaclav.koukola10@gmail.com>
2026-04-20 21:04:21 +0200

    Test

--- src/main/java/asmedit/gui/MachineWindow.java
+++ src/main/java/asmedit/gui/MachineWindow.java
@@ -62,6 +62,7 @@ public class MachineWindow extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
+    
     public static void main(String args[]) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
